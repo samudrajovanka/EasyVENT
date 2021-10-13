12960b152e93fc06164ce6387660ac8a03dbd686
Jovanka <51015175+samudrajovanka@users.noreply.github.com>
2021-10-13 19:49:29 +0700

    fix(card-event): fix no header event list in profile page (#8)

--- components/EventList/index.jsx
+++ components/EventList/index.jsx
@@ -3,11 +3,11 @@ import CardEvent from '@components/CardEvent';
 import PropTypes from 'prop-types';
 import uuid from 'react-uuid';
 
-export default function EventList({ events }) {
+export default function EventList({ events, noHeader }) {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {events.map((event) => (
-        <CardEvent key={uuid()} event={event} />
+        <CardEvent key={uuid()} event={event} noHeader={noHeader} />
       ))}
     </div>
   );
